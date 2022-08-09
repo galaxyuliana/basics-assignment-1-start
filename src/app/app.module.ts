8a27943d246d7b685aeb028528cc4588fb104ac6
galaxyuliana <yulianashin613@gmail.com>
2022-08-08 22:11:03 -0700

    Added success-alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
